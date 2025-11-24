055695d9d07d9e01e8df8f7d87faddfaac531d3e
unknown <kupzsensei>
2025-11-24 13:54:30 +0800

    Updated class

--- class.py
+++ class.py
@@ -1,27 +1,48 @@
 from dataclasses import dataclass
+from enum import Enum
+
+class AbilityType(Enum):
+    DAMAGE = "Damage"
+    HEAL = "Heal"
+    DEBUff = "Debuff"
+    SHIELD = "Shield"
+
 
 @dataclass
-class Person:
+class Ability:
     name: str
-    age: int
-    hobbies: list
-    is_alive: bool = True
+    type: AbilityType
+    value: float
 
     def __str__(self):
-        return " ".join(self.name.split()).title()
-    
-    def play(self, index: int = 0):
-        limit = len(self.hobbies) - 1
+        return self.name
 
-        if index > limit:
-            return print("Index must be within 0 to ", limit)
-        
-        if index < 0:
-            return print("Index must not be less than 0")
-
-        return self.hobbies[index]
 
 @dataclass
-class Donsal(Person):
-    profession: str
-    eabab: list
+class Pokemon:
+    name: str
+    level: int = 1
+    xp: float = 0
+    max_hp: int = 100
+    current_hp: int = max_hp
+    abilities: list
+
+    def __str__(self):
+        return self.name
+    
+    def useAbility(self, target: object, ability):
+        return f"{self.name} used {ability} {target}"
+
+
+# PICACHU SET
+# Pica Abilities
+thunder_bolt = Ability("Thunder Bolt", AbilityType.DAMAGE, 14.5)
+
+picachu = Pokemon("Picachu", abilities=[thunder_bolt])
+
+
+
+balbasaur = Pokemon("Balbasaur", xp=90)
+
+print(picachu.useAbility(balbasaur))
+# print(balbasaur)
